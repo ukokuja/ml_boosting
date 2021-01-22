65ac07216628d60a0dc614c54f9c479bb19cdde7
ukokuja <ukokuja@gmail.com>
2021-01-22 12:51:49 +0200

    Adds documentation and enhance quality code

--- boosting.py
+++ boosting.py
@@ -17,57 +17,15 @@ class Boosting():
         self.accuracy = []
         self.predictions = []
 
-    def fit(self):
-        self._set_all_examples_to_have_equal_uniform_weights()
 
-        X = self.data.drop(['result'], axis=1)
-        Y = self.data['result'].where(self.data['result'] == 1, 0)
-
-        for i in range(0, self.iterations):
-            tree_model = DecisionTreeClassifier(criterion="entropy", max_depth=1)
-            model = tree_model.fit(X, Y, sample_weight=np.array(self._evaluation['weights']))
-            self.models.append(model)
-
-            predictions = model.predict(X)
-            self._evaluation['predictions'] = predictions
-            self._evaluation['evaluation'] = np.where(self._evaluation['predictions'] == self.data['result'], 1, 0)
-            self._evaluation['misclassified'] = np.where(self._evaluation['predictions'] != self.data['result'], 1, 0)
-
-            accuracy = sum(self._evaluation['evaluation']) / len(self._evaluation['evaluation'])
-            misclassification = sum(self._evaluation['misclassified']) / len(self._evaluation['misclassified'])
-
-            err = np.sum(self._evaluation['weights'] * self._evaluation['misclassified']) / np.sum(
-                self._evaluation['weights'])
-
-            alpha = np.log((1 - err) / err)
-            self.alphas.append(alpha)
-
-            self._evaluation['weights'] *= np.exp(alpha * self._evaluation['misclassified'])
-
-
-    def predict(self):
-        X_test = self.test.drop(['result'], axis=1).reindex(range(len(self.test)))
-        Y_test = self.test['result'].reindex(range(len(self.test))).where(self.data['result'] == 1, 0)
-
-        # With each model in the self.model list, make a prediction 
-
-        predictions = []
-
-        for alpha, model in zip(self.alphas, self.models):
-            prediction = alpha * model.predict(X_test)
-            predictions.append(prediction)
-            self.accuracy.append(
-                np.sum(np.sign(np.sum(np.array(predictions), axis=0)) == Y_test.values) / len(predictions[0]))
-        self.predictions = np.sign(np.sum(np.array(predictions), axis=0))
-        self._output_result(self.predictions)
-
-    def _set_all_examples_to_have_equal_uniform_weights(self):
-        self._evaluation = pd.DataFrame(self.data.result.copy())
-        self._evaluation['weights'] = 1 / len(self.data)
-        self._evaluation.drop('result', inplace=True, axis=1)
 
     @staticmethod
     def _preprocess(df):
+        """
+        Transforms nominal data to ordinal and boolean
+        :param df: df to transform
+        :return:
+        """
         df['result'] = np.where(df['survived'] == 'yes', 1, 0)
         df['is_female'] = np.where(df['gender'] == 'female', 1, 0)
         df['is_child'] = np.where(df['age'] == 'child', 1, 0)
@@ -78,17 +36,121 @@ class Boosting():
 
         df.drop(columns=['gender', 'age', 'survived'], inplace=True)
 
+    def fit(self):
+        """
+        Trains the model based on dataset
+        :return:
+        """
+        self._set_all_examples_to_have_equal_uniform_weights()
+
+        X = self.data.drop(['result'], axis=1)
+        Y = self.data['result'].where(self.data['result'] == 1, 0)
+
+        for i in range(0, self.iterations):
+            model = self._train_model(X, Y)
+
+            # Add ht to H
+            self.models.append(model)
+
+            self._evaluation['predictions'] = model.predict(X)
+            self._evaluation['is_prediction_incorrect'] = np.where(
+                self._evaluation['predictions'] != self.data['result'], 1, 0)
+
+            error_rate = self._get_error_rate()
+
+            # If εt > 0.5 then exit loop, else continue
+            if error_rate > 0.5:
+                continue
+
+            beta = self._get_beta(error_rate)
+            alpha = self._get_alpha(beta)
+            self.alphas.append(alpha)
+
+            self._recalculate_weights(beta)
+            self._rescale_weights()
+
+    def _set_all_examples_to_have_equal_uniform_weights(self):
+        self._evaluation = pd.DataFrame(self.data.result.copy())
+        self._evaluation['weights'] = 1 / len(self.data)
+        self._evaluation.drop('result', inplace=True, axis=1)
+
+    def _train_model(self, X, Y):
+        """
+        Trains model based using Tree Stump based on X and Y
+        :param X: Dataset without result parameter
+        :param Y: Result parameter
+        :return: Trained model
+        """
+
+        # Learn a hypothesis, ht, from the weighted examples
+        tree_model = DecisionTreeClassifier(criterion="entropy", max_depth=1)
+        model = tree_model.fit(X, Y, sample_weight=np.array(self._evaluation['weights']))
+        return model
+
+    def _get_error_rate(self):
+        # Calculate the error, εt, of the hypothesis ht as the total sum weight of the examples
+        # that it classifies incorrectly
+        err = np.sum(
+            self._evaluation['weights'] * self._evaluation['is_prediction_incorrect'])
+        return err
+
+
+    @staticmethod
+    def _get_alpha(beta):
+        # αt =0.5*ln(1/ βt)
+        return 0.5 * np.log(1 / beta)
+
+    @staticmethod
+    def _get_beta(error_rate):
+        # Let βt = εt / (1 – εt )
+        return error_rate / (1 - error_rate)
+
+    def _recalculate_weights(self, alpha):
+        # Multiply the weights of the examples that
+        # ht classifies correctly by βt
+        self._evaluation['weights'] *= np.exp(alpha * self._evaluation['is_prediction_incorrect'])
+
+    def _rescale_weights(self):
+        # Rescale the weights of all of the examples so the total sum weight remains 1
+        self._evaluation['weights'].div(self._evaluation['weights'].sum())
+
+
+    def predict(self):
+        """
+        Predict based on previous generated models and alphas
+        :return:
+        """
+        X_test = self.test.drop(['result'], axis=1).reindex(range(len(self.test)))
+
+        predictions = []
+
+        for alpha, model in zip(self.alphas, self.models):
+            prediction = alpha * model.predict(X_test)
+            predictions.append(prediction)
+        self.predictions = np.sign(np.sum(np.array(predictions), axis=0))
+        self._output_result(self.predictions)
+
+
 
     def _output_result(self, result):
-
+        """
+        Outputs the result
+        :param result: prediction result
+        :return:
+        """
         self._output_success(result)
 
         self._clean_for_output()
 
-        self.test.drop(columns=['result', 'is_female', 'is_child', 'result', 'corrects_rows'], inplace=True)
         self.test.to_csv('titanikPrediction.csv', index=False)
+        print(self.test)
 
     def _output_success(self, result):
+        """
+        Calculates the percentage of success based on prediction results
+        :param result: prediction result
+        :return:
+        """
         self.test['pred'] = result
         self.test['corrects_rows'] = result == self.test.result
         corrects_rows = np.sum(self.test['corrects_rows'].astype(int))
@@ -96,15 +158,21 @@ class Boosting():
         print("Success: {}%".format(corrects_rows * 100 / n))
 
     def _clean_for_output(self):
+        """
+        Transform tests data as it was originally before pre-process
+        :return:
+        """
         self.test['survived'] = np.where(self.test['result'] == 1, 'yes', 'no')
         self.test['gender'] = np.where(self.test['is_female'] == 1, 'female', 'male')
         self.test['age'] = np.where(self.test['is_child'] == True, 'child', 'adult')
         self.test['pclass'] = self.test['pclass'].replace({0: '1st', 1: '2nd', 2: '3rd', 3: 'crew'})
         self.test['pred'] = np.where(self.test['pred'] == 1, 'yes', 'no')
 
+        self.test.drop(columns=['result', 'is_female', 'is_child', 'result', 'corrects_rows'], inplace=True)
+
 
 if __name__ == "__main__":
-    b = Boosting(pd.read_csv('titanikData.csv'), pd.read_csv('titanikTest.csv', names=["pclass", "age", "gender", "survived"]))
+    b = Boosting(pd.read_csv('titanikData.csv'),
+                 pd.read_csv('titanikTest.csv', names=["pclass", "age", "gender", "survived"]))
     b.fit()
     b.predict()
-    pass
